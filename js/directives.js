3b49a9357a8a0d9c3cdf1df9ba1c8b54f127161f
denise <denise.j.ferreira@gmail.com>
2014-03-22 17:02:53 -0300

    Criado template para pilha de cartas usando diretiva

---
+++ js/directives.js
@@ -0,0 +1,17 @@
+  
+angular.module('klondike', ['klondike.directive']).
+run(function() {
+});
+
+
+angular.module('klondike.directive', []).directive(
+	'cardPile', function() {
+	return {
+	  	templateUrl: 'templates/cardPile.html',
+	    scope: {
+		    'text': '@',
+		    'type': '@'
+		},
+	  	restrict: "E"
+	};
+ });
---
+++ js/greetingController.js
@@ -0,0 +1,6 @@
+function GreetingController($scope) {  
+    $scope.customer = {
+      name: 'Naomi',
+      address: '1600 Amphitheatre'
+    };
+}
