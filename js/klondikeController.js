2f8998c9499fbbb3a2dd4f82f223490850c2e8b8
Elias Dorneles <eliasdorneles@gmail.com>
2014-03-22 21:12:32 -0300

    rascunho da inicializacao do jogo

--- js/klondikeController.js
+++ js/klondikeController.js
@@ -5,22 +5,48 @@ function Card(suit, number) {
     this.number = number;
 }
 
-function Deck() {
-    this.cards = [];
+function createDeck() {
+    var cards = [];
+    angular.forEach(Suits, function(suit) {
+        angular.forEach(["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"], function(number){
+            cards.push(new Card(suit, number));
+        })
+    });
+    return shuffle(cards);
 
-    var $deck = this;
-    this.init = function() {
-        $deck.cards = [];
-        angular.forEach(Suits, function(suit) {
-            angular.forEach(["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"], function(number){
-                $deck.cards.push(new Card(suit, number));
-            })
-        });
+    function shuffle(array) {
+        var counter = array.length, temp, index;
+        while (counter > 0) {
+            index = Math.floor(Math.random() * counter);
+            counter--;
+            temp = array[counter];
+            array[counter] = array[index];
+            array[index] = temp;
+        }
+
+        return array;
     }
 }
 
+function Klondike() {
+    this.stock = [];
+    this.waste = [];
+    this.foundations = [[], [], [], []];
+    this.tableaus = [];
+    var klondike = this;
+
+    this.init = function(){
+        var deck = createDeck();
+        angular.forEach([1, 2, 3, 4, 5, 6, 7], function(numCards){
+            klondike.tableaus.push(deck.splice(0, numCards));
+        });
+        klondike.stock = deck;
+    };
+}
+
 function KlondikeController($scope) {  
-    $scope.deck = new Deck();
-    $scope.deck.init();
-    console.log('deck', $scope.deck);
+    $scope.game = new Klondike();
+    $scope.game.init();
+
+    console.log('game', $scope.game);
 }
