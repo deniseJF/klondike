b6956c46eaf1a6925294468587d61ffcc7987164
denise <denise.j.ferreira@gmail.com>
2014-03-25 22:46:56 -0300

    adicionado rascunho de drag

--- js/directives.js
+++ js/directives.js
@@ -15,4 +15,35 @@ angular.module('klondike.directive', []).directive(
 		},
 	  	restrict: "E"
 	};
- });
+ }).directive('draggable', function($document) {
+    return function(scope, element, attr) {
+      var startX = 0, startY = 0, x = 0, y = 0;
+      element.css({
+       position: 'relative',
+       cursor: 'pointer'
+      });
+      element.on('mousedown', function(event) {
+        // Prevent default dragging of selected content
+        y = parseInt(angular.element(element[0]).css('top'));
+        event.preventDefault();
+        startX = event.screenX - x;
+        startY = event.screenY - y;
+        $document.on('mousemove', mousemove);
+        $document.on('mouseup', mouseup);
+      });
+
+      function mousemove(event) {
+        y = event.screenY - startY;
+        x = event.screenX - startX;
+        element.css({
+          top: y + 'px' ,
+          left:  x + 'px'
+        });
+      }
+
+      function mouseup() {
+        $document.unbind('mousemove', mousemove);
+        $document.unbind('mouseup', mouseup);
+      }
+    };
+  });
